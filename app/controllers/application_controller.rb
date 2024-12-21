a01c4e19cae660cf34de8c93b143582a636fd758
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-12-21 12:39:29 -0500

    home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,46 @@
 require 'sinatra/base'
 
 class ApplicationController < Sinatra::Base
-  # Establece el directorio de las vistas
-  set :views, File.expand_path('../../views', __FILE__)
-  # Establece la carpeta de archivos estáticos
-  set :public_folder, File.expand_path('../../../public', __FILE__)
   # Habilitar logging
   configure :development, :production do
     enable :logging
   end
 
-  # Antes de cada petición, configura el logger
+  enable :sessions # Habilitar sesiones
+
+  configure do
+    set :session_secret, 'a4b89e6d2f4c7b98334f5e2c1e93460b2f94b24a6c9e5d073c44d4e69e839485'
+    set :sessions, expire_after: 3600
+    set :views, File.expand_path('../../views', __FILE__)
+    set :public_folder, File.expand_path('../../../public', __FILE__)
+  end
+
   before do
     env["rack.logger"] = Logger.new(STDOUT)
   end
 
+  before do
+    public_routes = ['/login', '/signup']
+    unless public_routes.include?(request.path_info) || session[:logged]
+      redirect '/login'
+    end
+  end
+
+  not_found do
+    extensions = ['css', 'js', 'png', ]
+    path = request.path.split('.')
+    if !extensions.include? path[path.length - 1]
+      status 404
+      '404: Recurso no encontrado'
+    end
+  end
+
   get '/' do
-    puts 'base'
-    erb :'home/index'
+    erb :'home/index', layout: :'layouts/application'
+  end
+
+  get '/sign-out' do
+    session.clear
+    redirect '/login'
   end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,9 +1,29 @@
 class LoginController < ApplicationController
   get '/login' do
     locals = { 
-      title: "Bienvenido a la página de inicio de sesión", 
-      user: "Usuario demo" 
+      title: 'Bienvenido a la página de inicio de sesión', 
+      user: 'Usuario demo',
+      error: false
     }
-    erb :'login/index', layout: :'layouts/blank', locals: 
+    erb :'login/index', layout: :'layouts/blank', locals: locals
+  end
+
+  post '/login' do
+    username = params[:username] # Capturar el nombre de usuario
+    password = params[:password] # Capturar la contraseña
+
+    # Validación básica (sustituir con la lógica de autenticación real)
+    if username == 'admin' && password == 'sistema123'
+      session[:logged] = true
+      redirect '/'  # Redirige a una página de éxito
+    else
+      error = 'Usuario o contraseña incorrectos'
+      locals = { 
+        title: 'Bienvenido a la página de inicio de sesión', 
+        user: 'Usuario demo',
+        error: error
+      }
+      erb :'login/index', layout: :'layouts/blank',  locals: locals
+    end
   end
 end
