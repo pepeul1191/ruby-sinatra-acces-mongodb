5f9a29b613e1f7d605fcf23a51d20c24fa694194
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-12-21 10:29:56 -0500

    archivos estaticos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
 
 class ApplicationController < Sinatra::Base
+  # Establece el directorio de las vistas
   set :views, File.expand_path('../../views', __FILE__)
+  # Establece la carpeta de archivos estáticos
+  set :public_folder, File.expand_path('../../../public', __FILE__)
 
   get '/' do
-    puts '++++++++++++++++++++++++'
     erb :index
   end
 end
