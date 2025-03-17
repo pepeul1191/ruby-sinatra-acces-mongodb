52d7f65e9d1e3987b8465c933098f870a311347b
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-03-16 22:09:40 -0500

    actualizar usuario

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -193,4 +193,41 @@ class UserController < ApplicationController
       redirect "users/edit/#{params[:_id]}?status=error&message=Ocurrió un error al actualizar la activación del usuario."
     end
   end
+
+  post '/users/:_id' do
+    begin
+      user = User.find(params[:_id])
+      updated_fields = {}
+      # check name is being updated and if it already exists for another user
+      if params[:name] && params[:name] != user.name
+        if User.where(name: params[:name]).exists?
+          redirect "/users/edit/#{params[:_id]}?status=error&message=El nombre de usuario ya está en uso"
+          return
+        end
+        updated_fields[:name] = params[:name]
+      end
+      # check email is being updated and if it already exists for another user
+      if params[:email] && params[:email] != user.email
+        if User.where(email: params[:email]).exists?
+          redirect "/users/edit/#{params[:_id]}?status=error&message=El correo electrónico ya está en uso"
+          return
+        end
+        updated_fields[:email] = params[:email]
+      end
+      # check new password
+      if params[:password] && params[:password] != user.email
+        updated_fields[:password] = params[:password]
+      end
+      # update
+      updated_fields[:updated] = Time.now
+      user.update!(updated_fields)  # Actualiza al usuario con los nuevos campos
+      redirect "/users/edit/#{params[:_id]}?status=success&message=Usuario actualizado correctamente"
+    rescue Mongoid::Errors::DocumentNotFound
+      redirect "/users/?status=error&message=No se encontró el usuario con el ID especificado"
+    rescue => e
+      puts "Error: #{e.message}"
+      puts e.backtrace
+      redirect "/users/edit/#{params[:_id]}?status=error&message=Ocurrió un error al actualizar el usuario"
+    end
+  end  
 end
